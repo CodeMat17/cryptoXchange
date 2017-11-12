cfc7d73259fff6ba38eb898d613df207e2cf3e8e
Chukwu Matthew <matthewnfvcb@gmail.com>
2017-11-12 02:22:41 +0100

    Initial commit

---
+++ app/src/main/java/com/matthew/cryptoxchange/About.java
@@ -0,0 +1,62 @@
+package com.matthew.cryptoxchange;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.view.MenuItem;
+import android.view.View;
+
+public class About extends AppCompatActivity {
+
+    @Override
+    public void onBackPressed(){
+        super.onBackPressed();
+        startActivity(new Intent(About.this, MainActivity.class));
+        finish();
+    }
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        ActionBar ab = getSupportActionBar();
+        ab.setTitle(Html.fromHtml("<font color=#ffffff>" + getString(R.string.app_name) + "</font>"));
+        //ab.setIcon(getResources().getDrawable(R.drawable.ic_action_back)); white back button
+        if(ab != null) {
+            ab.setDisplayHomeAsUpEnabled(true);
+
+        }
+
+        View mainContent = findViewById(R.id.main_content);
+        if (mainContent != null) {
+            mainContent.setAlpha(0);
+            mainContent.animate().alpha(1).setDuration(2000);
+        }
+
+    }
+
+    /* @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }*/
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if (id == android.R.id.home) {
+            startActivity(new Intent(About.this, MainActivity.class));
+            finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
